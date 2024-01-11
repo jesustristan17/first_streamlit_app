09d89b591e1b5d1c3710e4fc3328cff85b0ee7ec
jesustristan17 <156007139+jesustristan17@users.noreply.github.com>
2024-01-11 17:11:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.write('The user entered ', fruit_choice)
 # takes the JSON version and normalize ir
 
 #Don't run anything past here while we troubleshot
-streamlit.stop()
+#streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
